fafc654791a753e4dc4fa27332058875b0235340
Lyndon Ho <44751422+Dingledooper@users.noreply.github.com>
2019-08-01 21:28:41 -0700

    Update Print_Digit.asm

--- Print_Digit.asm
+++ Print_Digit.asm
@@ -6,6 +6,9 @@ section .text
         
 _start:
         mov rax, 7                      ; set the digit to print as '7'
+        call _printDigit
+
+_printDigit:
         add rax, 48                     ; 48 is the value of the character '0' in ASCII, '1' is 49...
         mov [digit], al                 ; move lower byte of rax register to address 'digit'
         
